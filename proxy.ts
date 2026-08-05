71362d4220433b51490f9a27a889de2fedb3e1f2
tarek fawzy <tf5234045@gmail.com>
2026-08-06 02:10:13 +0300

    optimize proxy navigation by using cookies

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getAuthSession } from "./lib/auth-server";
+import { decode } from "./lib/preference";
+import { auth } from "./lib/auth";
 
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  const session = await getAuthSession();
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  const isProtected =
+    pathname.startsWith("/admin") || pathname.startsWith("/provider");
 
   if (!session) {
-    return NextResponse.redirect(new URL("/not-found", request.url));
+    if (isProtected) {
+      return NextResponse.redirect(new URL("/not-found", request.url));
+    }
+
+    return NextResponse.next();
   }
 
   const role = session.user?.role;
+
+  if (pathname === "/") {
+    const checker = decode(
+      request.cookies.get("wl_auto_open_dashboard")?.value,
+    );
+
+    if (checker && role === "admin") {
+      return NextResponse.redirect(new URL("/admin", request.url));
+    }
+
+    if (checker && role === "provider") {
+      return NextResponse.redirect(new URL("/provider", request.url));
+    }
+
+    return NextResponse.next();
+  }
 
   if (pathname.startsWith("/admin") && role !== "admin") {
     return NextResponse.redirect(new URL("/unauthorized", request.url));
@@ -23,5 +49,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/provider/:path*"],
+  matcher: ["/", "/admin/:path*", "/provider/:path*"],
 };
